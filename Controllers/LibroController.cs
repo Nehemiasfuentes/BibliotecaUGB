53d195fd2c27756c38cb2309d1506aa541f6d498
Geovany Romero <romerogeovanny08@gmail.com>
2021-12-12 22:13:02 -0600

    Geovanny Agregando los verbos http get post al controlador de libro

--- Controllers/LibroController.cs
+++ Controllers/LibroController.cs
@@ -14,5 +14,36 @@ namespace BibliotecaUGB.Controllers
     [Route("[controller]")]
     public class LibroController : Controller
     {
+         private DatabaseContext _context;
+    public LibroController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Libro>>> ObtenerListaLibros()
+    {
+        var libros = await _context.Libros.ToListAsync();
+        return libros;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Libro>> ObtenerLibroPorID(int id_libro)
+    {
+        var libros = await _context.Libros.FindAsync(id_libro);
+        if(libros==null)
+        {
+            return NotFound();
+        }
+        return libros;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Libro>> PublicarLibro(Libro libro)
+    {
+        _context.Libros.Add(libro);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("ObtenerLibroPorID", new{id_libro=libro.LibroID},libro);
+    }
     }
 }
