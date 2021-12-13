45ec98a13d783ebf96056079ded85feb4696e752
Nehemias Fuentes <isaifuentes2017@gmail.com>
2021-12-12 23:07:08 -0600

    Archivos de docker

---
+++ Migrations/20211213050118_Migrations.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BibliotecaUGB.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    AutorID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NombresAutor = table.Column<string>(type: "text", nullable: true),
+                    ApellidosAutor = table.Column<string>(type: "text", nullable: true),
+                    EdadAutor = table.Column<string>(type: "text", nullable: true),
+                    PaisAutor = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.AutorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    LibroID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TituloLibro = table.Column<string>(type: "text", nullable: true),
+                    PortadaLibro = table.Column<string>(type: "text", nullable: true),
+                    ISBLibro = table.Column<string>(type: "text", nullable: true),
+                    CategoriaLibro = table.Column<string>(type: "text", nullable: true),
+                    Descripcion = table.Column<string>(type: "text", nullable: true),
+                    AutorID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.LibroID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+
+            migrationBuilder.DropTable(
+                name: "Libros");
+        }
+    }
+}
